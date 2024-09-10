d516c6966ecdc16e3d5d50b8490813959a1c6062
Ndèye Awa Goumbala Sambe <goumbalasambe@gmail.com>
2024-09-10 10:44:03 +0000

    erreur

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ class JeemaCoder extends React.Component {
       editIndex: -1,
     };
     this.handleClick = this.handleClick.bind(this);
-    this.handleDelete = this.handleDelete.bind(this)
-    this.handleEdit = this.handleEdit.bind(this)
+    this.goumbalaHandleClickEdit = this.goumbalaHandleClickEdit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   handleClick() {
@@ -23,38 +24,55 @@ class JeemaCoder extends React.Component {
       telephone: this.state.telephoneInput,
     };
 
+    // if (this.state.editIndex > -1) {
+    //   const updatedCoders = [...this.state.coders];
+    //   updatedCoders[this.state.editIndex] = newCoder;
+    //   this.setState({ coders: updatedCoders, editIndex: -1 });
+    // } else {
+
+    // }
+    // ajouter un nouveau codeur
+    this.setState({ coders: [newCoder, ...this.state.coders] });
+    // vider le formulaire
+    this.setState({
+      prenomInput: "",
+      nomInput: "",
+      emailInput: "",
+      telephoneInput: "",
+    });
+  }
+
+  handleDelete(index) {
+    const updatedCoders = this.state.coders.filter((_, i) => i !== index);
+    this.setState({ coders: updatedCoders });
+  }
+
+  handleEdit(index) {
+    // Recuperation du coder selectionne
+    const coder = this.state.coders[index];
+    this.setState({
+      prenomInput: coder.prenom,
+      nomInput: coder.nom,
+      emailInput: coder.email,
+      telephoneInput: coder.telephone,
+      editIndex: index,
+    });
+  }
+
+  goumbalaHandleClickEdit() {
+    const modifCoder = {
+      prenom: this.state.prenomInput,
+      nom: this.state.nomInput,
+      email: this.state.emailInput,
+      telephone: this.state.telephoneInput,
+    };
+
     if (this.state.editIndex > -1) {
       const updatedCoders = [...this.state.coders];
-      updatedCoders[this.state.editIndex] = newCoder;
-      this.setState({ coders: updatedCoders, editIndex: -1});
-    } else {
-      // ajouter un nouveau codeur
-      this.setState({ coders: [newCoder, ...this.state.coders] });
+      updatedCoders[this.state.editIndex] = modifCoder;
+      this.setState({ coders: updatedCoders, editIndex: -1 });
     }
-      // vider le formulaire
-      this.setState({
-        prenomInput: "",
-        nomInput: "",
-        emailInput: "",
-        telephoneInput: "",
-      });
-    }
-
-    handleDelete(index) {
-      const updatedCoders = this.state.coders.filter((_, i) => i !== index)
-      this.setState({ coders: updatedCoders})
-    }
-
-    handleEdit(index) {
-      const coder = this.state.coders[index];
-      this.setState({
-        prenomInput: coder.prenom,
-        nomInput: coder.nom,
-        emailInput: coder.email,
-        telephoneInput: coder.telephone,
-        editIndex: index,
-      })
-    }
+  }
 
   render() {
     return (
@@ -111,12 +129,21 @@ class JeemaCoder extends React.Component {
                 />
               </div>
             </div>
-            <button
-              onClick={this.handleClick}
-              className="btn btn-outline-primary w-100 mt-3"
-            >
-              {this.state.editIndex > -1 ? "Modifier" : "Submit"}
-            </button>
+            {this.state.editIndex > -1 ? 
+              <button
+                onClick={this.goumbalaHandleClickEdit}
+                className="btn btn-outline-primary w-100 mt-3"
+              >
+                Modifier
+              </button>
+             : 
+              <button
+                onClick={this.handleClick}
+                className="btn btn-outline-primary w-100 mt-3"
+              >
+                Submit
+              </button>
+            }
           </div>
         </div>
         <div className="mt-5 container">
@@ -129,7 +156,6 @@ class JeemaCoder extends React.Component {
                 <th scope="col">Email</th>
                 <th scope="col">Telephone</th>
                 <th scope="col">Actions</th>
-
               </tr>
             </thead>
             <tbody>
@@ -144,12 +170,14 @@ class JeemaCoder extends React.Component {
                       <button
                         onClick={() => this.handleEdit(index)}
                         className="btn btn-outline-warning mx-1"
-                      >Modifier
+                      >
+                        Modifier
                       </button>
                       <button
                         onClick={() => this.handleDelete(index)}
                         className="btn btn-outline-danger mx-1"
-                      >Supprimer
+                      >
+                        Supprimer
                       </button>
                     </td>
                   </tr>
